dba8527c2d96c840e8b444940464bd8d77e327fe
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-12-05 18:13:11 +0900

    commit

---
+++ electron/main.cjs
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.cjs")
+        }
+    });
+
+    if (isDev) {
+        win.loadURL("http://localhost:5173");
+    } else {
+        win.loadFile(path.join(__dirname, "../dist/index.html"));
+    }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
