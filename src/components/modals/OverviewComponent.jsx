5f81a55f8b7f2b071f3b8eb896aed9ed7e89ab6f
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-12-08 17:04:21 +0900

    오버뷰 페이지: 이미지보다 글자가 렌더링되는 현상 수정

--- src/components/modals/OverviewComponent.jsx
+++ src/components/modals/OverviewComponent.jsx
@@ -1,28 +1,42 @@
 import React, {useEffect, useState} from 'react';
 import './OverviewComponent.css'
 import Logo from '/img/overview/over-logo.svg'
+import BgImage from '/img/overview/overview-back.png'
 
 function OverviewComponent({isOpen}) {
+    const [imageLoaded, setImageLoaded] = useState(false);
     const [showContent, setShowContent] = useState(false);
 
     useEffect(() => {
-        if (isOpen) {
-            // 열릴 때 3초 뒤에 show true
-            const timer = setTimeout(() => {
-                setShowContent(true);
-            }, 1000);
 
-            return () => clearTimeout(timer); // cleanup
-        } else {
-            // 닫힐 때 초기화
-            setShowContent(false);
-        }
+        if (!isOpen) return;
+
+        const img = new Image();
+        img.src = BgImage;
+        img.onload = () => {
+            setImageLoaded(true);
+        };
+
     }, [isOpen]);
+
+    useEffect(() => {
+
+        if (!imageLoaded) return;
+
+        const timer = setTimeout(() => {
+            setShowContent(true);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+
+    }, [imageLoaded]);
+
+    if (!isOpen) return null; // 닫힐 때는 컴포넌트 자체가 없어짐
 
     return (
         <>
             {isOpen && (
-                <div id="overview-wrap">
+                <div id="overview-wrap"  style={{ backgroundImage: `url(${BgImage})` }}>
                     <div className={`contents ${showContent ? "show" : ""}`}>
                         <h2>Hydrogen Powered Solution Provider</h2>
                         <p>Doosan aims to supply clean energy throughout human daily life by<br />
