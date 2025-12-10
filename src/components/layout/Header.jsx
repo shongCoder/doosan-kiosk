2347926b2d0ffdd3a3595db94c71513efc9e2d41
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-12-10 13:27:11 +0900

    특징 페이지 애니메이션 중간 커밋

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 import "./Header.css"
 import Logo from "/doosan-logo.svg"
 import BackBtn from "/img/header/back-btn.svg"
+import { motion } from "framer-motion";
 
 function Header({ title, isBack, onBackClick }) {
+
     return (
         <>
             <div id="header">
                 {/* 필요할 때만 버튼 노출 */}
                 {isBack ? (
-                    <button style={{ marginRight: "-4.25rem" }} onClick={onBackClick}>
+                    <motion.button
+                        style={{ marginRight: "-4.25rem" }}
+                        onClick={onBackClick}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 2, duration: 0.4, ease: "easeOut" }} // 딜레이만 적용!
+                    >
                         <img src={BackBtn} />
-                    </button>
+                    </motion.button>
                 ) : (<div style={{width: "80px", marginRight: "-4.25rem"}}></div>)}
 
                 {title === "" ?
-                    (<img src={Logo} />) : (<h2 className="header-title">{title}</h2>)}
-
-
+                    (<img src={Logo} />) : (
+                        <motion.h2
+                            className="header-title"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 2, duration: 0.4, ease: "easeOut" }}
+                        >
+                            {title}
+                        </motion.h2>
+                    )}
                 <div></div>
             </div>
         </>
--- src/components/modals/MajorFeaturesComponent.jsx
+++ src/components/modals/MajorFeaturesComponent.jsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {motion} from 'framer-motion';
 import './MajorFeaturesComponent.css'
 import "@fontsource/tomorrow/700.css";
 import PEMFC from "/img/features/PEMFC.png"
@@ -8,156 +9,167 @@ import AirCooling from "/img/features/Air-Cooling-type.png"
 import DS035CCD from "/img/features/DS035CCD.png"
 import BottomText from "/img/features/bottom-text.svg"
 import DetailComponent from "./features/DetailComponent";
+import {AnimatePresence} from "framer-motion";
 
 function MajorFeaturesComponent({isOpen}) {
     const [toggle, setToggle] = useState(0);
-    const [visible, setVisible] = useState(isOpen);
-    const [displayToggle, setDisplayToggle] = useState(0);
-    const [fade, setFade] = useState("fade-in");
     const [product, setProduct] = useState("");
 
-    // 모달
-    const [isOpenPemfc, setIsOpenPemfc] = useState(false);
+    const [isOpenDetail, setIsOpenDetail] = useState(false);
+
+    const quickEase = [0.17, 0.67, 0.83, 0.67];
+
+    const handleToggle = (page) => {
+        setToggle(page);
+    }
 
     useEffect(() => {
-        if (isOpen) {
-            setVisible(true);
-        } else {
-            const timer = setTimeout(() => setVisible(false), 400);
-            return () => clearTimeout(timer);
+        if(!isOpen) {
+            setToggle(0);
         }
     }, [isOpen]);
 
-    const handleToggle = (val) => {
-        if (val === toggle) return;
-
-        setFade("fade-out"); // 먼저 기존 콘텐츠를 fade-out
-
-        setTimeout(() => {
-            setDisplayToggle(val); // 디졸브 끝난 후 콘텐츠 교체
-            setFade("fade-in");    // 새 콘텐츠 fade-in
-            setToggle(val);
-        }, 350); // CSS 트랜지션 시간과 동일하게
-    };
-
     return (
         <>
-            <DetailComponent isOpen={isOpenPemfc} onClose={() => setIsOpenPemfc(false)} product={product} />
-            {visible && (
-                <div id="feature-wrap" className={isOpen ? 'fade-in' : 'fade-out'}>
-                    <div className="overlay">
-                        <div className="menu-toggle-box" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>
-                            <div
-                                className={`menu-toggle ${toggle === 0 ? "active" : ""}`}
-                                onClick={() => handleToggle(0)}
-                            >
-                                Fuel Cell Sysyems
+            <DetailComponent isOpen={isOpenDetail} onClose={() => setIsOpenDetail(false)} product={product} />
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        id="feature-wrap"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{
+                            duration: 0.8,
+                            ease: "easeOut"
+                        }}
+                    >
+                        <div className="overlay">
+                            <div className="menu-toggle-box" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>
+                                <div
+                                    className={`menu-toggle ${toggle === 0 ? "active" : ""}`}
+                                    onClick={() => handleToggle(0)}
+                                >
+                                    Fuel Cell Sysyems
+                                </div>
+                                <div
+                                    className={`menu-toggle ${toggle === 1 ? "active" : ""}`}
+                                    onClick={() => handleToggle(1)}
+                                >
+                                    Hydrogen Drones
+                                </div>
                             </div>
-                            <div
-                                className={`menu-toggle ${toggle === 1 ? "active" : ""}`}
-                                onClick={() => handleToggle(1)}
-                            >
-                                Hydrogen Drones
-                            </div>
-                        </div>
-                        {/* Toggle Box */}
+                            {/* Toggle Box */}
 
-                        {toggle === 0 ? (
-                            <div className={`contents ${fade}`}>
-                                <div className="title-box">
-                                    <div className="sub-title">Major Features</div>
-                                    <div className="title">Fuel Cell Sysyems</div>
-                                </div>
-                                {/* Title */}
-                                <div className="cont-wrap">
-                                    <div className="cont-box">
-                                        <div className="cont-title">
-                                            <div className="dot" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>∙</div>
-                                            <div className="main" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>Power Generation</div>
-                                            <div className="line">|</div>
-                                            <div className="qty">2 Products</div>
-                                        </div>
-                                        <div className="cont-card">
-                                            <button
-                                                onClick={() => {
-                                                    setIsOpenPemfc(true);
-                                                    setProduct("PEMFC");
-                                                }}
-                                            >
-                                                <img src={PEMFC} />
-                                            </button>
-                                            <button
-                                                onClick={() => {
-                                                    setIsOpenPemfc(true);
-                                                    setProduct("SOFC");
-                                                }}
-                                            >
-                                                <img src={SOFC} />
-                                            </button>
-                                        </div>
+                            {toggle === 0 ? (
+                                <motion.div
+                                    key="contents0"
+                                    className="contents"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{ duration: 0.3, ease: quickEase }}
+                                >
+                                    <div className="title-box">
+                                        <div className="sub-title">Major Features</div>
+                                        <div className="title">Fuel Cell Sysyems</div>
                                     </div>
-                                    <div className="cont-box">
-                                        <div className="cont-title">
-                                            <div className="dot" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>∙</div>
-                                            <div className="main" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>Mobility</div>
-                                            <div className="line">|</div>
-                                            <div className="qty">2 Products</div>
-                                        </div>
-                                        <div className="cont-card">
-                                            <button
-                                                onClick={() => {
-                                                    setIsOpenPemfc(true);
-                                                    setProduct("Water Cooling type");
-                                                }}
-                                            >
-                                                <img src={WaterCooling} />
-                                            </button>
-                                            <button
-                                                onClick={() => {
-                                                    setIsOpenPemfc(true);
-                                                    setProduct("Air Cooling type");
-                                                }}
-                                            >
-                                                <img src={AirCooling} />
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ):(
-                            <div className={`contents sec ${fade}`}>
-                                <div className="title-box">
-                                    <div className="sub-title">Major Features</div>
-                                    <div className="title">Hydrogen Drones</div>
-                                </div>
-                                {/* Title */}
-                                <div className="cont-wrap">
-                                    <div className="cont-box">
-                                        <div className="card-wrap">
+                                    {/* Title */}
+                                    <div className="cont-wrap">
+                                        <div className="cont-box">
                                             <div className="cont-title">
                                                 <div className="dot" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>∙</div>
-                                                <div className="qty">1 Products</div>
+                                                <div className="main" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>Power Generation</div>
+                                                <div className="line">|</div>
+                                                <div className="qty">2 Products</div>
                                             </div>
                                             <div className="cont-card">
                                                 <button
-                                                    className="sec"
                                                     onClick={() => {
-                                                        setIsOpenPemfc(true);
-                                                        setProduct("DS035CCD");
+                                                        setIsOpenDetail(true);
+                                                        setProduct("PEMFC");
                                                     }}
                                                 >
-                                                    <img src={DS035CCD} />
+                                                    <img src={PEMFC} />
+                                                </button>
+                                                <button
+                                                    onClick={() => {
+                                                        setIsOpenDetail(true);
+                                                        setProduct("SOFC");
+                                                    }}
+                                                >
+                                                    <img src={SOFC} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div className="cont-box">
+                                            <div className="cont-title">
+                                                <div className="dot" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>∙</div>
+                                                <div className="main" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>Mobility</div>
+                                                <div className="line">|</div>
+                                                <div className="qty">2 Products</div>
+                                            </div>
+                                            <div className="cont-card">
+                                                <button
+                                                    onClick={() => {
+                                                        setIsOpenDetail(true);
+                                                        setProduct("Water Cooling type");
+                                                    }}
+                                                >
+                                                    <img src={WaterCooling} />
+                                                </button>
+                                                <button
+                                                    onClick={() => {
+                                                        setIsOpenDetail(true);
+                                                        setProduct("Air Cooling type");
+                                                    }}
+                                                >
+                                                    <img src={AirCooling} />
                                                 </button>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                    <img className="bottom-text" src={BottomText} />
-                </div>
-            )}
+                                </motion.div>
+                            ):(
+                                <motion.div
+                                    key="contents1"
+                                    className="contents sec"
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{ duration: 0.3, ease: quickEase }}
+                                >
+                                    <div className="title-box">
+                                        <div className="sub-title">Major Features</div>
+                                        <div className="title">Hydrogen Drones</div>
+                                    </div>
+                                    {/* Title */}
+                                    <div className="cont-wrap">
+                                        <div className="cont-box">
+                                            <div className="card-wrap">
+                                                <div className="cont-title">
+                                                    <div className="dot" style={{ fontFamily: "Tomorrow", fontWeight: 700 }}>∙</div>
+                                                    <div className="qty">1 Products</div>
+                                                </div>
+                                                <div className="cont-card">
+                                                    <button
+                                                        className="sec"
+                                                        onClick={() => {
+                                                            setIsOpenDetail(true);
+                                                            setProduct("DS035CCD");
+                                                        }}
+                                                    >
+                                                        <img src={DS035CCD} />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </div>
+                        <img className="bottom-text" src={BottomText} />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </>
     );
 }
